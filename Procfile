5afbc4bacc902128a9d04ba5446590400d6803e5
leonwolfus <leonvolkov@gmail.com>
2017-01-09 22:57:17 +0200

    static again

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python ./manage.py collectstatic --noinput; uwsgi --http :$PORT --module wagtaildemo.heroku_wsgi --master --processes 2 --static-map /media/=/app/media/ --offload-threads 1
+web: uwsgi --http :$PORT --module wagtaildemo.heroku_wsgi --master --processes 2 --static-map /media/=/app/media/ --offload-threads 1
--- wagtaildemo/settings/base.py
+++ wagtaildemo/settings/base.py
@@ -87,6 +87,13 @@ STATICFILES_DIRS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
